dd84abef1f2da2c529b3f358d8739df6743b2a31
koro298 <89699953+koro298@users.noreply.github.com>
2022-09-01 23:29:56 +0900

    コントローラ変更

--- game_manager/block_controller.py
+++ game_manager/block_controller.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import pprint
 import copy
 import numpy
+from board_manager import BOARD_DATA
 
 class Block_Controller(object):
 
@@ -16,6 +17,12 @@ class Block_Controller(object):
     CurrentShape_class = 0
     NextShape_class = 0
 
+    index_tetris_raw = 10
+    flg_hidden_tetris = 0
+    flg_hole = 0
+    counter = 0
+    
+
     # GetNextMove is main function.
     # input
     #    nextMove : nextMove structure which is empty.
@@ -24,12 +31,12 @@ class Block_Controller(object):
     # output
     #    nextMove : nextMove structure which includes next shape position and the other.
     def GetNextMove(self, nextMove, GameStatus):
-
+        print(self.counter)
+        self.counter = self.counter+1
         t1 = datetime.now()
 
         # print GameStatus
         print("=================================================>")
-        pprint.pprint(GameStatus, width = 61, compact = True)
 
         # get data from GameStatus
         # current shape info
@@ -38,6 +45,12 @@ class Block_Controller(object):
         # next shape info
         NextShapeDirectionRange = GameStatus["block_info"]["nextShape"]["direction_range"]
         self.NextShape_class = GameStatus["block_info"]["nextShape"]["class"]
+        self.NextShape_class = GameStatus["block_info"]["nextShape"]["class"]
+
+        # 次のミノの種類を知りたいけど...　
+        NextShapeClass, NextShapeIdx, NextShapeRange = BOARD_DATA.getShapeData(1) # nextShape
+        print(NextShapeIdx)
+
         # current board info
         self.board_backboard = GameStatus["field_info"]["backboard"]
         # default board definition
@@ -45,12 +58,14 @@ class Block_Controller(object):
         self.board_data_height = GameStatus["field_info"]["height"]
         self.ShapeNone_index = GameStatus["debug_info"]["shape_info"]["shapeNone"]["index"]
 
+        # 現在の盤面を調査
+        board = copy.deepcopy(self.board_backboard)
+
         # search best nextMove -->
         strategy = None
         LatestEvalValue = -100000
         # search with current block Shape
         for direction0 in CurrentShapeDirectionRange:
-            print("CurrentShapeDirectionRange:",CurrentShapeDirectionRange)
             # search with x range
             x0Min, x0Max = self.getSearchXRange(self.CurrentShape_class, direction0)
             for x0 in range(x0Min, x0Max):
@@ -58,7 +73,7 @@ class Block_Controller(object):
                 board = self.getBoard(self.board_backboard, self.CurrentShape_class, direction0, x0)
 
                 # evaluate board
-                EvalValue = self.calcEvaluationValueSample(board)
+                EvalValue = self.calcEvaluationValueSample(board,self.NextShape_class)
                 # update best move
                 if EvalValue > LatestEvalValue:
                     strategy = (direction0, x0, 1, 1)
@@ -80,8 +95,7 @@ class Block_Controller(object):
         nextMove["strategy"]["x"] = strategy[1]
         nextMove["strategy"]["y_operation"] = strategy[2]
         nextMove["strategy"]["y_moveblocknum"] = strategy[3]
-        print(nextMove)
-        print("###### SAMPLE CODE ######")
+
         return nextMove
 
     def getSearchXRange(self, Shape_class, direction):
@@ -94,14 +108,12 @@ class Block_Controller(object):
         return xMin, xMax
 
     def getShapeCoordArray(self, Shape_class, direction, x, y):
-        #
         # get coordinate array by given shape.
         #
         coordArray = Shape_class.getCoords(direction, x, y) # get array from shape direction, x, y.
         return coordArray
 
     def getBoard(self, board_backboard, Shape_class, direction, x):
-        # 
         # get new board.
         #
         # copy backboard data to make new board.
@@ -111,7 +123,6 @@ class Block_Controller(object):
         return _board
 
     def dropDown(self, board, Shape_class, direction, x):
-        # 
         # internal function of getBoard.
         # -- drop down the shape on the board.
         # 
@@ -130,7 +141,6 @@ class Block_Controller(object):
         return _board
 
     def dropDownWithDy(self, board, Shape_class, direction, x, dy):
-        #
         # internal function of dropDown.
         #
         _board = board
@@ -139,8 +149,7 @@ class Block_Controller(object):
             _board[(_y + dy) * self.board_data_width + _x] = Shape_class.shape
         return _board
 
-    def calcEvaluationValueSample(self, board):
-        # 
+    def calcEvaluationValueSample(self, board, NextShape_class):
         # sample function of evaluate board.
         #
         width = self.board_data_width
@@ -229,9 +238,104 @@ class Block_Controller(object):
         score = score - nIsolatedBlocks * 15.0      # try not to make isolated block
         score = score - maxHeight**3/125            # maxHeight
         score = score - 5*absDy                     # 
+        # 次に死ぬときはスコアを最低にする
+        
 
 
 
         return score
 
+    def evalCurrentBoard(self, board, NextShape_class):
+        width = self.board_data_width
+        height = self.board_data_height
+
+        # evaluation paramters
+        ## lines to be removed
+        fullLines = 0
+        ## number of holes or blocks in the line.
+        nHoles, nIsolatedBlocks = 0, 0
+        ## absolute differencial value of MaxY
+        absDy = 0
+        ## how blocks are accumlated
+        BlockMaxY = [0] * width
+        holeCandidates = [0] * width
+        holeConfirm = [0] * width
+
+        ### check board
+        # each y line
+        for y in range(height - 1, 0, -1):
+            hasHole = False
+            hasBlock = False
+            # each x line
+            for x in range(width):
+                ## check if hole or block..
+                if board[y * self.board_data_width + x] == self.ShapeNone_index:
+                    # hole
+                    hasHole = True
+                    holeCandidates[x] += 1  # just candidates in each column..
+                else:
+                    # block
+                    hasBlock = True
+                    BlockMaxY[x] = height - y                # update blockMaxY
+                    if holeCandidates[x] > 0:
+                        holeConfirm[x] += holeCandidates[x]  # update number of holes in target column..
+                        holeCandidates[x] = 0                # reset
+                    if holeConfirm[x] > 0:
+                        nIsolatedBlocks += 1                 # update number of isolated blocks
+                        # print('x(-1):'+str(board[y * self.board_data_width + x-1])+'\n'+'x(+1):'+str(board[y * self.board_data_width + x+1])+'\n'+'y(-1):'+str(board[(y-1) * self.board_data_width + x])+'\n'+'y(+1):'+str(board[(y+1) * self.board_data_width + x])+'\n')
+                        # print(' '+str(board[(y-1) * self.board_data_width + x])+' '+'\n'+str(board[y * self.board_data_width + x-1])+str(board[y * self.board_data_width + x])+str(board[y * self.board_data_width + x+1])+'\n'+' ' +str(board[(y+1) * self.board_data_width + x]) +'\n')
+
+
+            if hasBlock == True and hasHole == False:
+                # filled with block
+                fullLines += 1
+            elif hasBlock == True and hasHole == True:
+                # do nothing
+                pass
+            elif hasBlock == False:
+                # no block line (and ofcourse no hole)
+                pass
+
+        
+        # nHoles
+        for x in holeConfirm:
+            nHoles += abs(x)
+
+        # absolute differencial value of MaxY
+        BlockMaxDy = []
+        for i in range(len(BlockMaxY) - 1):
+            val = BlockMaxY[i] - BlockMaxY[i+1]
+            BlockMaxDy += [val]
+        for x in BlockMaxDy:
+            absDy += abs(x)
+
+        # 最大連続穴（上空き）
+        # i_max = numpy.argmax(BlockMaxY)
+        # for i in range(len(BlockMaxDy) - 1):
+
+        # U字型
+
+
+        # maxDy
+        #maxDy = max(BlockMaxY) - min(BlockMaxY)
+        # maxHeight
+        maxHeight = max(BlockMaxY) - fullLines
+
+     
+        # 連続穴
+
+
+        # calc Evaluation Value
+        score = 0
+        score = score + numpy.exp(fullLines)        # try to delete line 
+        score = score - nHoles * 5.0                # try not to make hole
+        score = score - nIsolatedBlocks * 15.0      # try not to make isolated block
+        score = score - maxHeight**3/125            # maxHeight
+        score = score - 5*absDy                     # 
+        # 次に死ぬときはスコアを最低にする
+        
+
+
+
+        return score
 BLOCK_CONTROLLER = Block_Controller()
